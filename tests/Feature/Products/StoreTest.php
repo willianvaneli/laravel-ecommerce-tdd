225fd424a099755ba50996787c684042f78924ed
willian.vaneli <willianvaneli@gmail.com>
2022-01-07 13:37:38 -0300

    primeiro teste

--- tests/Feature/Products/StoreTest.php
+++ tests/Feature/Products/StoreTest.php
@@ -1,14 +1,15 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Feature\Products;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class StoreTest extends TestCase
 {
     /**
-     * A basic test example.
+     * A basic feature test example.
      *
      * @return void
      */
